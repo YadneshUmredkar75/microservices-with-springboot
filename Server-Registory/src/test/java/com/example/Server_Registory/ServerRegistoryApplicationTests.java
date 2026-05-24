b7d7515b8b7081422e195cfa88aeabd7a2528b15
YadneshUmredkar75 <yadneshumredkar75@gmail.com>
2026-05-24 18:03:41 +0530

    micro-service S-

---
+++ Server-Registory/src/test/java/com/example/Server_Registory/ServerRegistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Server_Registory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerRegistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
